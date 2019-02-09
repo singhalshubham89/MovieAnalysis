f33d868565470cf2dec90c372b91b3cd2c31e311
Shbham Singhal <singhalshubham89@gmail.com>
2019-02-09 18:49:57 +0530

    Completing basic and some advance functionality
    - Use of Web Api to fetch movie details
    - Implement a grid on the UI, listing all movie details.
    - Implement pagination on the UI. Show only 10 records at a time and allow users to go back and forth.
    
    - Grid columns should provide sort and filter like excel sheet.
    - Columns should provide sort and filter
    - The filter should be implemented for each individual column

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild  } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AgGridNg2 } from 'ag-grid-angular'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  {
+
+  @ViewChild('agGrid') agGrid: AgGridNg2;
+
   title = 'MovieAnalysis';
+
+    gridApi:any;
+    gridColumnApi:any;
+
+    columnDefs:any;
+    defaultColDef:any;
+    colResizeDefault:any;
+    rowData: any;
+
+  
+
+  
+
+
+
+  constructor( private http: HttpClient) {
+
+    this.columnDefs = [
+      { headerName: 'Movie ', field: 'movie_title' ,width: 150},
+      { headerName: 'Director', field: 'director_name' ,width: 130 },
+      { headerName: 'Actor 1', field: 'actor_1_name' ,width: 130 },
+      { headerName: 'Actor 2', field: 'actor_2_name' ,width: 130 },
+      { headerName: 'Genres', field: 'genres' ,width: 150 },
+      { headerName: 'Language', field: 'language'  ,width: 100},
+      { headerName: 'Country', field: 'country'  ,width: 70 },
+      { headerName: 'Rating', field: 'content_rating'  ,width: 70},
+      { headerName: 'Budget', field: 'budget' ,width: 100 },
+      { headerName: 'Title Year', field: 'title_year' ,width: 70 },
+      { headerName: 'Plot Keywords', field: 'plot_keywords' ,width: 150 },
+      { headerName: 'Movie Imb Link', field: 'movie_imdb_link'  ,width: 150},
+  
+    ];
+
+    this.defaultColDef = { resizable: true,sortable: true, filter: "agTextColumnFilter",floatingFilter: true};
+  
+    }
+
+
+  onGridReady(params) {
+    this.gridApi = params.api;
+    this.gridColumnApi = params.columnApi;
+
+    this.rowData=this.http.get("/movies");
+  }
+  
 }
